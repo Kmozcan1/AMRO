c4be0d34cc31d02a56d77b250e95602d1beea083
kadirmertozcan <kmozcan1@gmail.com>
2026-01-29 08:09:26 +0100

    + add bottom spacer to details

--- movies/ui/src/main/java/com/abn/amro/movies/ui/feature/detail/view/DetailContent.kt
+++ movies/ui/src/main/java/com/abn/amro/movies/ui/feature/detail/view/DetailContent.kt
@@ -9,9 +9,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -140,6 +142,8 @@ fun DetailContent(
                         .fillMaxWidth(),
                 )
             }
+
+            Spacer(Modifier.navigationBarsPadding())
         }
     }
 }
